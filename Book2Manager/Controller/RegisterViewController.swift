a5e70ff33d526e4d9e74ea817724cbaa128b280d
aiba <socceraba2701@gmail.com>
2022-04-25 13:32:36 +0900

    テキスト入力機能の追加

--- Book2Manager/Controller/RegisterViewController.swift
+++ Book2Manager/Controller/RegisterViewController.swift
@@ -9,7 +9,15 @@ import UIKit
 import AVFoundation
 import Foundation
 
-class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+extension UIScrollView {
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.superview?.touchesBegan(touches, with: event)
+            print("touches began")
+    }
+}
+
+class RegisterViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var TitleTextField: UITextField!
     
@@ -28,23 +36,80 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIImagePick
     
     @IBOutlet weak var CaptureView: UIView!
     
+    @IBOutlet weak var ScrollView: UIScrollView!
+    
     //model
     var barcodemodel = BarcodeModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        ScrollView.delegate = self
         TitleTextField.delegate = self
         AuthorTextField.delegate = self
         PublisherTextField.delegate = self
         CommentTextView.layer.cornerRadius = 5
         
+        textViewSetup()
         barcodemodel.setup(CaptureView, TitleTextField, AuthorTextField, PublisherTextField, CommentTextView, ImageView, RegisterButton)
        
     }
     
+    // textViewを閉じるためのボタンを生成
+    func textViewSetup(){
+        //ツールバー生成
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+        // スタイルを設定
+        toolBar.barStyle = UIBarStyle.default
+        // 画面幅に合わせてサイズを変更
+        toolBar.sizeToFit()
+        // 閉じるボタンを右に配置するためのスペース?
+        let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+        // 閉じるボタン
+        let commitButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(commitButtonTapped))
+        // スペース、閉じるボタンを右側に配置
+        toolBar.items = [spacer, commitButton]
+        // textViewのキーボードにツールバーを設定
+        CommentTextView.inputAccessoryView = toolBar
+    }
     
+    @objc func commitButtonTapped() {
+        self.view.endEditing(true)
+    }
+        
+    //=======textfield===============
+    //TextFieldリターンキーが押されたとき
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        //今フォーカスが当たっているテキストボックスからフォーカスを外す
+        textField.resignFirstResponder()
+        
+        //tileが入力されていたらボタンを有効にする
+        if let textname = TitleTextField.text{
+            if textname.count != 0{
+                RegisterButton.isEnabled = true
+            }else{
+                RegisterButton.isEnabled = false
+            }
+        }
+        
+        return true
+    }
     
+    //TextField入力中
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        //tileが入力されていたらボタンを有効にする
+        if let textname = TitleTextField.text{
+            if textname.count != 0{
+                RegisterButton.isEnabled = true
+            }else{
+                RegisterButton.isEnabled = false
+            }
+        }
+        return true
+    }
+
     @IBAction func ImageUploadAction(_ sender: Any) {
     }
     
