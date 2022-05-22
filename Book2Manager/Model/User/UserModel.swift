64d7cafb276cc00947eacef98f00298aaf3393b7
aiba <socceraba2701@gmail.com>
2022-05-23 05:53:19 +0900

    modified usermodel

--- Book2Manager/Model/User/UserModel.swift
+++ Book2Manager/Model/User/UserModel.swift
@@ -33,19 +33,6 @@ class UserModel{
     let existSemaphore = DispatchSemaphore(value: 0)
     var existState = ""
     
-    //更新ボタンを押した
-    func reLoad() -> String {
-        
-        let result = loadUserData()
-        
-        if result != "success" {
-            return result
-        }
-        useridx = [0, 1 ,2 ,3 ,4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-        
-        return "success"
-    }
-    
     //user.txtが存在するか確認
     func exist(){
         if DropboxClientsManager.authorizedClient == nil {
@@ -150,47 +137,6 @@ class UserModel{
         
     }
     
-    
-    
-    
-    func loadUserData()->String{
-        
-        guard let dirURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            return "フォルダURL取得エラー"
-        }
-        
-        //user.txtファイルがない場合は，usersを書き込み作成
-        if !FileManager.default.fileExists(atPath: filepath){
-            let fileURL = dirURL.appendingPathComponent(filename)
-            
-            FileManager.default.createFile(atPath: filepath, contents: nil, attributes: nil)
-    
-            var writetxt = users[0]
-            for i in 1 ..< users.count {
-                writetxt += "\n" + users[i]
-            }
-            do {
-                try writetxt.write(to: fileURL, atomically: false, encoding: .utf8)
-            } catch {
-                return "ファイル書き込みエラー"
-            }
-            
-        }else{
-        //user.txtが存在する場合はusersに読み込み
-            let fileURL = dirURL.appendingPathComponent(filename)
-
-            do {
-                let text = try String(contentsOf: fileURL)
-                users = text.components(separatedBy: "\n").filter{!$0.isEmpty}
-                
-            }catch {
-                return "ファイル読み込みエラー"
-            }
-        }
-        
-        return "success"
-    }
-    
     func swap(_ start : Int, _ end: Int){
         
         //一個ずつ左にする
@@ -210,25 +156,4 @@ class UserModel{
         }
     }
     
-    func save()->String{
-        
-        guard let dirURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            return "フォルダURL取得エラー"
-        }
-        
-        let fileURL = dirURL.appendingPathComponent(filename)
-        
-        var writetxt = users[useridx[0]]
-        for i in 1 ..< useridx.count {
-            writetxt += "\n" + users[useridx[i]]
-        }
-      
-        do {
-            try writetxt.write(to: fileURL, atomically: false, encoding: .utf8)
-        } catch {
-            return "ファイル書き込みエラー"
-        }
-
-        return "success"
-    }
 }
